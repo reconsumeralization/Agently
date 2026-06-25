5dbb7b35d2a62785be3806d6603d213128463a6f
Maplemx <maplemx@gmail.com>
2026-06-25 09:35:37 +0800

    feat: add resumable TaskBoard tick execution

--- agently/core/orchestration/TaskBoard/__init__.py
+++ agently/core/orchestration/TaskBoard/__init__.py
@@ -16,6 +16,7 @@ from .TaskBoardRuntime import (
     TaskBoard,
     TaskBoardContext,
     TaskBoardHandler,
+    TaskBoardTickExecution,
     TaskBoardTickResult,
 )
 from .TaskBoardEvidence import (
@@ -45,6 +46,7 @@ __all__ = [
     "TaskBoardEffortProfile",
     "TaskBoardEvidenceView",
     "TaskBoardHandler",
+    "TaskBoardTickExecution",
     "TaskBoardPlanningPolicy",
     "TaskBoardPlanningResult",
     "TaskBoardTickResult",
